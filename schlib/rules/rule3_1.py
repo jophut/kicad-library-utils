69906e267b4752f8ece1e5d6a92fdabe19e0c90d
Jop Huttenhuis <jophuttenhuis@gmail.com>
2017-02-20 21:52:25 +0100

    Implemented code to find pins with identical an coordinate

--- schlib/rules/rule3_1.py
+++ schlib/rules/rule3_1.py
@@ -23,7 +23,32 @@ class Rule(KLCRule):
                 self.violating_pins.append(pin)
                 self.verboseOut(Verbosity.HIGH, Severity.ERROR, 'pin: {0} ({1}), {2}'.format(pin['name'], pin['num'], positionFormater(pin)))
 
-        return True if len(self.violating_pins) > 0 else False
+	"""
+	Check if pins share the same coordinate
+	"""
+	# Sort the pin list by columns: posx, posy, num.
+	self.spl = sorted(self.component.pins, key=lambda x: tuple((x['posx'],x['posy'],x['num'])))
+
+	# Empty list to store the duplicates, each entry will become (again) a list of pin numbers.
+	self.dupes=[]
+	i=0 # Index iterate over all items, and easily refer to the next item.
+	while i < (len(self.spl)-1):	# Skip the last pin, implies lists of more than one element.
+		posx = self.spl[i]['posx'] # Keep track of the 'current' coordinate.
+		posy = self.spl[i]['posy']
+		if posx == self.spl[i+1]['posx'] and posy == self.spl[i+1]['posy']:
+			self.dupes.append([self.spl[i]['num']])	# Store the element of the matching pair
+
+			while posx == self.spl[i+1]['posx'] and posy == self.spl[i+1]['posy']: # Also store the second and possible more pins with the same coordinate
+				self.dupes[-1].append(self.spl[i+1]['num'])
+				i+=1 # Advance while keeping the current coordinate
+		i+=1 # Advance to the next pin (with a new coordinate)
+
+	if len(self.dupes):
+                self.verboseOut(Verbosity.HIGH, Severity.ERROR, 'Found more than one pin on the same coordinate(s):')
+		for d in self.dupes:
+			self.verboseOut(Verbosity.HIGH, Severity.ERROR, 'Pins {0} share the same coordinate'.format(d))
+
+        return True if len(self.violating_pins) > 0 or len(self.dupes) > 0 else False
 
     def fix(self):
         """
